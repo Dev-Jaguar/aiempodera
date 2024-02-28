628e04a2db1708c66c3dba96b7ca19b690f9cd0d
Emanuel Sarria <emamuel.sarria@icloud.com>
2024-02-28 17:12:21 -0500

    Ahora las personas pueden subir sus avatares, yayy

--- pages/admin_page.py
+++ pages/admin_page.py
@@ -3,10 +3,15 @@ import streamlit as st
 st.title("Hello, Admin")
 
 st.info("We will only admit the script in .txt")
-uploaded_files = st.file_uploader("Upload the script", type=['.txt'], accept_multiple_files=True)
+uploaded_scripts = st.file_uploader("Upload your script", type=['.txt'], accept_multiple_files=True)
 
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("Filename: ", uploaded_file.name)
+for uploaded_script in uploaded_scripts:
+    bytes_data = uploaded_script.read()
+    st.write("Filename: ", uploaded_script.name)
     st.write("Content: ", bytes_data)
 
+st.divider()
+
+uploaded_avatar = st.file_uploader("Upload your avatar", type=['.jpg', '.png'], accept_multiple_files=False)
+st.write("Name of the avatar: ", uploaded_avatar)
+st.image(uploaded_avatar)
